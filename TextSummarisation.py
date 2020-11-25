24cef77f12a6e57158d48232c5c76df187da5c96
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-11-25 16:37:54 +0000

    Update TextSummarisation.py

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -5,6 +5,8 @@ summarizer = pipeline("summarization")
 
 st.title('Text Summarisation by Yulei')
 text = st.text_area('Please type the texts you want to summarise here.')
+if not text:
+    st.stop()
 summary = summarizer(text[:1024], max_length=120, min_length=30, do_sample=False)
 
 st.header('Please see your summarisation below:')
