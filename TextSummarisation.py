cbf7b4c48f799925eb8b4705b4b4e006872be146
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-12-02 16:47:36 +0000

    Update TextSummarisation.py

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -1,12 +1,15 @@
 import streamlit as st
 from transformers import pipeline
-summarizer = pipeline("summarization")
+@st.cache
+def sum():
+    summarizer = pipeline("summarization")
+    return summarizer
 
 st.title('Text Summarisation by Yulei')
 text = st.text_area('Please type the texts you want to summarise here.')
 if not text:
     st.stop()
-summary = summarizer(text[:1024], max_length=120, min_length=30, do_sample=False)
+summary = sum(text[:1024], max_length=120, min_length=30, do_sample=False)
 
 st.header('Please see your summarisation below:')
 st.write(summary[0]['summary_text'])
