b286c8078ebde8c943aac33ad688c9b8cae0581b
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-12-02 16:52:01 +0000

    Update TextSummarisation.py

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -14,5 +14,5 @@ st.header('Please see your summarisation below:')
 st.write(summary(text))
 
 from deep_translator import GoogleTranslator
-translated = GoogleTranslator(source='auto', target='zh-cn').translate(summary[0]['summary_text'])
+translated = GoogleTranslator(source='auto', target='zh-cn').translate(summary(text))
 st.write(translated)
