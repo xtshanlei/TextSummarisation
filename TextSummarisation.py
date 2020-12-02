058e122c73a6a51b97d897a33c6b02a1eb993180
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-12-02 16:48:41 +0000

    Update TextSummarisation.py

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -3,13 +3,13 @@ from transformers import pipeline
 @st.cache
 def sum():
     summarizer = pipeline("summarization")
-    return summarizer
 
 st.title('Text Summarisation by Yulei')
 text = st.text_area('Please type the texts you want to summarise here.')
 if not text:
     st.stop()
-summary = sum(text[:1024], max_length=120, min_length=30, do_sample=False)
+sum()
+summary = summarizer(text[:1024], max_length=120, min_length=30, do_sample=False)
 
 st.header('Please see your summarisation below:')
 st.write(summary[0]['summary_text'])
