c88f829ab3bf8b811e2946753c3377427597d0b0
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-12-02 16:53:57 +0000

    Update TextSummarisation.py

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from transformers import pipeline
 @st.cache
-def summary(text):
+def summary():
     summarizer = pipeline("summarization")
-    summary = summarizer(text[:1024], max_length=120, min_length=30, do_sample=False)
-    return summary[0]['summary_text']
+    return summarizer
 st.title('Text Summarisation by Yulei')
 text = st.text_area('Please type the texts you want to summarise here.')
 if not text:
     st.stop()
+summary = summary()(text[:1024], max_length=120, min_length=30, do_sample=False)
 
 st.header('Please see your summarisation below:')
-st.write(summary(text))
+st.write(summary[0]['summary_text'])
 
 from deep_translator import GoogleTranslator
 translated = GoogleTranslator(source='auto', target='zh-cn').translate(summary(text))
