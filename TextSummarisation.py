89fe8a62d8fafc2d684c02ea5801cd3047ae378b
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2020-12-02 14:42:06 +0000

    Update TextSummarisation.py

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from transformers import pipeline
+import pydeepl
+
+sentence = 'I like turtles.'
+from_language = 'EN'
+to_language = 'ES'
+
+translation = pydeepl.translate(sentence, to_language, from_lang=from_language)
+print(translation)
+
+# Using auto-detection
+translation = pydeepl.translate(sentence, to_language)
+print(translation)
 summarizer = pipeline("summarization")
 
 st.title('Text Summarisation by Yulei')
@@ -10,3 +22,9 @@ summary = summarizer(text[:1024], max_length=120, min_length=30, do_sample=False
 
 st.header('Please see your summarisation below:')
 st.write(summary[0]['summary_text'])
+
+sentence = 'I like turtles.'
+to_language = 'CH'
+
+translation = pydeepl.translate(sentence, to_language)
+print(translation)
