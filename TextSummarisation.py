22512c4a90b0736070ab7d40fba1b2a1d33966a6
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-11-09 19:40:53 +0000

    asdf

--- TextSummarisation.py
+++ TextSummarisation.py
@@ -1,17 +1,2 @@
 import streamlit as st
-from transformers import pipeline
-def summary(text):
-    summarizer = pipeline("summarization")
-    summary = summarizer(text[:1024], max_length=120, min_length=30, do_sample=False)
-    return summary[0]['summary_text']
-st.title('Text Summarisation by Yulei')
-text = st.text_area('Please type the texts you want to summarise here.')
-if not text:
-    st.stop()
-
-st.header('Please see your summarisation below:')
-st.write(summary(text))
-
-from deep_translator import GoogleTranslator
-translated = GoogleTranslator(source='auto', target='zh-cn').translate(summary(text))
-st.write(translated)
+import spacy_streamlit
